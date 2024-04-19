701e44688736488156a804613712877f76756290
mahyarmoghadaam <Mahyar.Moghadam@outlook.com>
2024-04-19 09:38:13 +0330

    Initial Commit

---
+++ GizoSDKCocoapods/GizoSDKCocoapods.h
@@ -0,0 +1,18 @@
+//
+//  GizoSDKCocoapods.h
+//  GizoSDKCocoapods
+//
+//  Created by Meysam Farmani on 4/19/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GizoSDKCocoapods.
+FOUNDATION_EXPORT double GizoSDKCocoapodsVersionNumber;
+
+//! Project version string for GizoSDKCocoapods.
+FOUNDATION_EXPORT const unsigned char GizoSDKCocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GizoSDKCocoapods/PublicHeader.h>
+
+
